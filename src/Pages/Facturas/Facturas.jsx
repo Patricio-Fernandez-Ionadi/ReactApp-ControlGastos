61d5408e70b66f5bbf4fcced2973bac132fcaf59
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2020-11-13 18:03:04 -0300

    inicio facturas pag

--- src/Pages/Facturas/Facturas.jsx
+++ src/Pages/Facturas/Facturas.jsx
@@ -2,8 +2,20 @@ import React from "react"
 // Components
 import HeaderPages from "../../components/HeaderPages/HeaderPages"
 import HeroPages from "../../components/HeaderPages/HeroPages"
+// Styles
+import { makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles((theme) => ({
+	listContainer: {
+		width: "90%",
+		minHeight: "800px",
+		backgroundColor: "#ccc",
+		margin: "auto",
+	},
+}))
 
 export default function Facturas() {
+	const classes = useStyles()
 	return (
 		<>
 			<HeaderPages titleofpage='Facturas' />
@@ -15,6 +27,8 @@ export default function Facturas() {
 
 			<div>
 				<h1>Listado de facturas</h1>
+
+				<div className={classes.listContainer}></div>
 			</div>
 		</>
 	)
